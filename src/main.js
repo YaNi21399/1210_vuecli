925b715bfba07cc87044bdc80eaa2f8a60890325
YaNi21399 <90896873+YaNi21399@users.noreply.github.com>
2021-12-24 22:51:59 +0800

    currency

--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import 'vue-loading-overlay/dist/vue-loading.css'
 
 import App from './App.vue'
 import router from './router'
+import { currency } from './methods/filter'
 
 const app = createApp(App)
+app.config.globalProperties.$filters = {
+  currency
+}
 app.use(VueAxios, axios)
 app.use(router)
 app.component('Loading', Loading)
